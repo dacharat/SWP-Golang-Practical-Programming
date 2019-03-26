b8782bad74467a1cee25f59bf709f25be8e2156c
dacharat <dacharat.p@ku.th>
2019-03-26 11:59:46 +0700

    :newspaper: part3 add unmodified version

--- code/part3/exercise-web-crawler.go
+++ code/part3/exercise-web-crawler.go
@@ -10,78 +10,26 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
-// Add
-func CrawlRecursive(url string, depth int, fetcher Fetcher, quit chan bool, visitedUrls map[string]bool) {
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
 	if depth <= 0 {
-		quit <- true
 		return
 	}
-
-	didIt, hasIt := visitedUrls[url]
-	// If we have already visited this link,
-	// stop here
-	if didIt && hasIt {
-		quit <- true
-		return
-
-	} else {
-		// Mark it has visited
-		visitedUrls[url] = true
-	}
-
-	// Fetch children URLs
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
-		quit <- true
 		return
 	}
-	fmt.Printf("found URL: %s ; title: %q\n", url, body)
-
-	// Crawl children URLs
-	childrenQuit := make(chan bool)
-	for _, childrenUrl := range urls {
-		go CrawlRecursive(childrenUrl, depth-1, fetcher, childrenQuit, visitedUrls)
-		// To exit goroutines. This channel will always be filled
-		<-childrenQuit
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
 	}
-
-	quit <- true
+	return
 }
-
-func Crawl(url string, depth int, fetcher Fetcher) {
-	quit := make(chan bool)
-	// Say we haven't visited the first URL yet
-	visitedUrls := map[string]bool{url: false}
-
-	// Le'ts go, crawl from the given URL
-	go CrawlRecursive(url, depth, fetcher, quit, visitedUrls)
-
-	// We will not quit until we have something
-	// in the "quit" channel
-	<-quit
-}
-
-// // Crawl uses fetcher to recursively crawl
-// // pages starting with url, to a maximum of depth.
-// func Crawl(url string, depth int, fetcher Fetcher) {
-// 	// TODO: Fetch URLs in parallel.
-// 	// TODO: Don't fetch the same URL twice.
-// 	// This implementation doesn't do either:
-// 	if depth <= 0 {
-// 		return
-// 	}
-// 	body, urls, err := fetcher.Fetch(url)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	fmt.Printf("found: %s %q\n", url, body)
-// 	for _, u := range urls {
-// 		Crawl(u, depth-1, fetcher)
-// 	}
-// 	return
-// }
 
 func main() {
 	Crawl("https://golang.org/", 4, fetcher)
